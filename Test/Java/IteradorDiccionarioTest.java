ff5e870a8ea3b2640b4550577e5276570c336d74
DaniOrtegaR13 <d.ortegar@edu.uah.es>
2025-03-12 13:41:29 +0100

    getKey revisado

--- Test/Java/IteradorDiccionarioTest.java
+++ Test/Java/IteradorDiccionarioTest.java
@@ -10,18 +10,28 @@ class IteradorDiccionarioTest {
 
     @Test
     void next() {
+
     }
 
     @Test
     void getKey() {
+        ElementoDiccionario<String,Integer> elemento = new ElementoDiccionario<>("elemento",5);
+        DiccionarioBasico<String,Integer> diccionario = new DiccionarioBasico(elemento);
+        assertEquals("elemento", elemento.getKey());
+
     }
 
     @Test
     void getValue() {
+
     }
 
     @Test
     void getActual() {
+        ElementoDiccionario<String,Integer> elemento = new ElementoDiccionario<>("elemento",5);
+        DiccionarioBasico<String,Integer> diccionario = new DiccionarioBasico(elemento);
+        IteradorDiccionario<String, Integer> it = new IteradorDiccionario<>();
+        assertEquals(cew, diccionario.getActual());
     }
 
     @Test
