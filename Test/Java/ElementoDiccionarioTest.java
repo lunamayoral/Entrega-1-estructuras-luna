0b3e44720f416ecd8f896b33d295df4c0aefc8ad
Usuario <irene.martingarcia@edu.uah>
2025-03-12 13:45:06 +0100

    ElementoDiccionario

---
+++ Test/Java/ElementoDiccionarioTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElementoDiccionarioTest {
+    private ElementoDiccionario<String, Integer> elemento;
+
+    @Test
+    void delete() {
+        ElementoDiccionario<String, Integer> elemento = new ElementoDiccionario<>("elemento", 120);
+        ElementoDiccionario<String, Integer> elementoAnterior = new ElementoDiccionario<>("anterior", 456);
+        ElementoDiccionario<String, Integer> elementoSiguiente = new ElementoDiccionario<>("siguiente", 789);
+        elemento.anterior = elementoAnterior;
+        elemento.siguiente = elementoSiguiente;
+        elementoAnterior.siguiente = elemento;
+        elementoSiguiente.anterior = elemento;
+        assertTrue(elemento.delete());
+        assertTrue(elementoAnterior.siguiente==elementoSiguiente);
+        assertNull(elementoAnterior.anterior);
+        assertNull(elementoSiguiente.siguiente);
+        assertTrue(elementoSiguiente.delete());
+        assertTrue(elementoAnterior.delete());
+        elementoSiguiente=null;
+        elementoAnterior=null;
+        assertTrue(elementoSiguiente==null);
+        assertTrue(elementoAnterior==null);
+    }
+
+    @Test
+    void getKey() {
+        ElementoDiccionario<String, Integer> elemento = new ElementoDiccionario<>("elemento");
+        assertEquals("elemento", elemento.getKey());
+
+
+    }
+
+    @Test
+    void getValue() {
+        ElementoDiccionario<String, Integer> elemento = new ElementoDiccionario<>("elemento", 120);
+        assertEquals(120, elemento.getValue());
+
+    }
+
+    @Test
+    void setValue() {
+        ElementoDiccionario<String, Integer> elemento = new ElementoDiccionario<>("elemento", 120);
+        elemento.setValue(230);
+        assertEquals(230, elemento.getValue());
+
+    }
+}
