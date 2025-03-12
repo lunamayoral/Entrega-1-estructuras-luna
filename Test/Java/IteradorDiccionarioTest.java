9ad9379521fd6a0f8e5405a28c90b7bd1ac5706c
Samug <samuel.garciabarrio@edu.uah.es>
2025-03-12 13:49:41 +0100

    IteradorDiccionario test excepto delete

--- Test/Java/IteradorDiccionarioTest.java
+++ Test/Java/IteradorDiccionarioTest.java
@@ -6,24 +6,43 @@ class IteradorDiccionarioTest {
 
     @Test
     void hasNext() {
+        ElementoDiccionario<String, Integer> elemento = new ElementoDiccionario<>("elem", 5);
+        DiccionarioBasico<String, Integer> diccionario = new DiccionarioBasico<>(elemento);
+        IteradorDiccionario<String, Integer> it = new IteradorDiccionario<>(elemento);
+        assertFalse(it.hasNext());
+        diccionario.add("clave",12);
+        assertTrue(it.hasNext());
     }
 
     @Test
     void next() {
-
+        ElementoDiccionario<String, Integer> elemento = new ElementoDiccionario<>("elem", 5);
+        DiccionarioBasico<String, Integer> diccionario = new DiccionarioBasico<>(elemento);
+        diccionario.add("clave",12);
+        IteradorDiccionario<String, Integer> it = new IteradorDiccionario<>(elemento);
+        assertEquals("clave",it.next().getKey());
+        assertEquals(12,it.getValue());
+        assertEquals(null,it.next());
     }
 
     @Test
     void getKey() {
         ElementoDiccionario<String,Integer> elemento = new ElementoDiccionario<>("elemento",5);
         DiccionarioBasico<String,Integer> diccionario = new DiccionarioBasico(elemento);
-        assertEquals("elemento", elemento.getKey());
-
+        IteradorDiccionario<String, Integer> it = new IteradorDiccionario<>(elemento);
+        assertEquals("elemento", it.getKey());
+        IteradorDiccionario<String, Integer> it2 = new IteradorDiccionario<>();
+        assertEquals(null,it2.getKey());
     }
 
     @Test
     void getValue() {
-
+        ElementoDiccionario<String, Integer> elemento = new ElementoDiccionario<>("elem", 5);
+        DiccionarioBasico<String, Integer> diccionario = new DiccionarioBasico<>(elemento);
+        IteradorDiccionario<String, Integer> it = new IteradorDiccionario<>(elemento);
+        assertEquals(5,it.getValue());
+        IteradorDiccionario<String, Integer> it2 = new IteradorDiccionario<>();
+        assertEquals(null,it2.getValue());
     }
 
     @Test
@@ -31,7 +50,7 @@ class IteradorDiccionarioTest {
         ElementoDiccionario<String,Integer> elemento = new ElementoDiccionario<>("elemento",5);
         DiccionarioBasico<String,Integer> diccionario = new DiccionarioBasico(elemento);
         IteradorDiccionario<String, Integer> it = new IteradorDiccionario<>();
-        assertEquals(cew, diccionario.getActual());
+        //assertEquals(cew, diccionario.getActual());
     }
 
     @Test
